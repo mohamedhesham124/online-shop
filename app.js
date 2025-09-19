04738a02e2c476b77156d375bc4125000954d8b4
Mohamed Hesham <129544346+mohamedhesham124@users.noreply.github.com>
2025-09-19 20:48:14 +0300

    modify app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const SessionStore = require('connect-mongodb-session')(session);
+const flash = require('connect-flash')
+
+const app = express();
+const homeRouter=require('./routes/home.router')
+const productRouter=require('./routes/product.router')
+const authRouter=require('./routes/auth.router')
+
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(flash())
+
+const STORE = new SessionStore({
+    uri: 'mongodb://localhost:27017/online-shop',
+    collection: 'sessions'
+});
+
+app.use(session({
+    secret: 'this is my secret secret to hash express sessions .....',
+    saveUninitialized: false,
+    resave: false,
+    store: STORE,
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views'); // default
+
+app.use(homeRouter)
+app.use(productRouter)
+app.use(authRouter)
+
+app.listen(3000, () => {
+    console.log('server listen on port 3000');
+});

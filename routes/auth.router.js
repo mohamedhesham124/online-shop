3ace7bfd56f36a0174216abc6cd72eddec6399da
Mohamed Hesham <129544346+mohamedhesham124@users.noreply.github.com>
2025-09-21 01:59:43 +0300

    Update signup post api

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser');
+const check = require('express-validator').check;
 
 const authController = require('../controllers/auth.controller');
 
 // signup
 router.get('/signup', authController.getSignup);
-router.post('/signup', bodyParser.urlencoded({extended : true}), authController.postSignup);
+router.post(
+    "/signup",
+    bodyParser.urlencoded({extended: true}),
+    check("username").not().isEmpty(),
+    check("email").not().isEmpty().isEmail(),
+    check("password").isLength({min: 6, max:30}),
+    check("confirmPassword").custom((value, {req}) => {
+        if (value === req.body.password) return true;
+        else throw 'passwords dont equal';
+    }),
+    authController.postSignup
+);
 
 //login
 router.get('/login', authController.getLogin);
@@ -13,5 +25,6 @@ router.post('/login', bodyParser.urlencoded({extended : true}), authController.p
 
 //logout
 router.all('/logout', authController.logout);
+
 
 module.exports = router;
